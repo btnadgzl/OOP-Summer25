3ac6a6a02717a755068a680f6be8754360378ab7
Batin Adiguzel <batinadiguzel7@gmail.com>
2025-03-11 11:16:29 +0100

    Create basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,23 @@
+# Integer
+my_age = 20 # int  
+
+# Floating point number
+my_float = 9.1 # float  
+
+# String
+my_name = "Batın" # str
+
+# List
+my_favorite_numbers = [13, 6, 55] # list  
+
+# Dictionary
+my_profile = {"name": "batin", "age": "20"}  
+
+# Tuple
+my_coordinates = (78.8764, -45.9000)  # tup
+
+# Set
+my_unique_numbers = {6, 7, 8, 9}  # set
+
+# Boolean
+is_student = True  # bool
