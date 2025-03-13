d89e434255c435e873525c07afca57fa716d67b6
Batin Adiguzel <batinadiguzel7@gmail.com>
2025-03-13 10:59:52 +0100

    student_details.py

---
+++ student_details.py
@@ -0,0 +1,14 @@
+my_age = 20
+ my_indexnumber = 35134
+ my_name = "batin "
+ my_surname = "adiguzel"
+ nationality = "turja"
+ starting_date = "01.01.2025"
+courses = ["Mathematics", "Physics", "Computer Science"]
+ print(my_age,
+ my_indexnumber,
+ my_name ,
+ my_surname,
+ nationality,
+ starting_date,
+ courses)
