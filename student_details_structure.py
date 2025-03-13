9d40f13ac324ce16cd7c538bb17f88d61a271b24
Batin Adiguzel <batinadiguzel7@gmail.com>
2025-03-13 11:09:48 +0100

    Create student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,14 @@
+student1 = {
+'first_name' = 'Batin',
+'last_name' = 'Adiguzel',
+'index_number' = '35134',
+'nationality' = 'Turkey',
+'starting_date' = '2025-01-01',
+'courses' = ['Computer security', 'Match','Ethics']
+}
+print(student1.first_name)
+print(student1.last_name)
+print(student1.index_number)
+print(student1.nationality)
+print(student1.starting_date)
+print(student1.courses)
