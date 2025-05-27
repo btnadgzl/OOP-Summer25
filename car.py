335353ca78adbd95e85d5286160db54dc91682b4
Batin Adiguzel <batinadiguzel7@gmail.com>
2025-05-27 11:31:56 +0000

    Add a new file

---
+++ car.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self, brand, engine_type):
+        self.brand = brand              # Public attribute
+        self.__engine_type = engine_type  # Private attribute
+
+    def show_engine_type(self):
+        print(f"The engine type is: {self.__engine_type}")
+
+    def __start_the_engine(self):
+        print(f"The {self.brand} engine has started.")
+
+    def start_the_car(self):
+        self.__start_the_engine()
